70f53d37328ebd72957464717a4d7eac3c486ad4
MMarioB <mmariob196@gmail.com>
2024-12-01 12:30:45 +0100

    refactor login feature

--- cypress/e2e/features/login.feature
+++ cypress/e2e/features/login.feature
@@ -1,21 +1,21 @@
 Feature: Swag Labs User Authentication
-  As a Swag Labs admin
-  I want to log in and log out with different user types
-  So that I can verify the authentication functionality
+  As a Swag Labs standard user
+  I want to authenticate into the system
+  So that I can access the platform
 
   Scenario Outline: Successful login for different user types
     Given I am on the login page
-    When I login with "<username>" and "<password>"
-    Then I should be redirected to the inventory page
-    And I should see the products title
+    When I enter username "<username>" and password "<password>"
+    Then I should be logged in successfully
+    And I should see the products page
 
     Examples:
       | username                | password     |
-      | standard_user           | secret_sauce |
-      | problem_user            | secret_sauce |
-      | performance_glitch_user | secret_sauce |
+      | standard_user          | secret_sauce |
+      | problem_user           | secret_sauce |
+      | performance_glitch_user| secret_sauce |
 
   Scenario: Failed login for locked out user
     Given I am on the login page
-    When I login with "locked_out_user" and "secret_sauce"
-    Then I should see an error message indicating the account is locked
+    When I enter username "locked_out_user" and password "secret_sauce"
+    Then I should see error message "Epic sadface: Sorry, this user has been locked out."
--- cypress/e2e/step_definitions/loginSteps.js
+++ cypress/e2e/step_definitions/loginSteps.js
@@ -1,25 +1,35 @@
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
-import LoginPage from '../../support/pages/LoginPage';
-import InventoryPage from '../../support/pages/InventoryPage';
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('I am on the login page', () => {
-  LoginPage.visit();
+Given("I am on the login page", () => {
+  cy.visit("/");
+  cy.get('#user-name').should('be.visible');
+  cy.get('#password').should('be.visible');
 });
 
-When('I login with {string} and {string}', (username, password) => {
-  LoginPage.login(username, password);
+When("I enter username {string} and password {string}", (username, password) => {
+  // Using ID selectors for better reliability
+  cy.get('#user-name')
+    .should('have.attr', 'placeholder', 'Username')
+    .type(username);
+    
+  cy.get('#password')
+    .should('have.attr', 'placeholder', 'Password')
+    .type(password);
+    
+  cy.get('#login-button')
+    .should('have.attr', 'type', 'submit')
+    .click();
 });
 
-Then('I should be redirected to the inventory page', () => {
-  InventoryPage.validateInventoryPage();
+Then("I should be logged in successfully", () => {
+  cy.url().should("include", "/inventory.html");
 });
 
-Then('I should see the products title', () => {
-  InventoryPage.validateProductsTitle();
+Then("I should see the products page", () => {
+  cy.get(".title").should("have.text", "Products");
+  cy.get(".inventory_item").should("exist");
 });
 
-Then('I should see an error message indicating the account is locked', () => {
-  LoginPage.getErrorMessage()
-    .should('be.visible')
-    .and('contain', 'Sorry, this user has been locked out');
+Then("I should see error message {string}", (message) => {
+  cy.get('[data-test="error"]').should("have.text", message);
 });
