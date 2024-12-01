19d657a1eaee800f4d70f2cf2cdb3cc8506c6d6a
MMarioB <mmariob196@gmail.com>
2024-12-01 15:27:36 +0100

    delete commands unnecesary

--- cypress/support/commands/completePurchaseCommands.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add("completeCheckoutForm", (firstName, lastName, postalCode) => {
-    cy.get('[data-test="firstName"]').type(firstName);
-    cy.get('[data-test="lastName"]').type(lastName);
-    cy.get('[data-test="postalCode"]').type(postalCode);
-    cy.get('[data-test="continue"]').click();
-  });
