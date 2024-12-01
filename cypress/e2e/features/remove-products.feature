4db1b36d26e10eb42463c3ba8403977229a2ce53
MMarioB <mmariob196@gmail.com>
2024-12-01 17:47:38 +0100

    fix user story 4 tests

--- cypress/e2e/features/remove-products.feature
+++ cypress/e2e/features/remove-products.feature
@@ -1,41 +1,45 @@
 Feature: Remove Products from Cart
+  As a standard user
+  I want to remove products from my cart
+  So that I can manage my purchase items
 
   Background:
-    Given I am logged in as a standard user
+    Given I visit the Swag Labs homepage
+    And I log in as standard user
 
   Scenario: Remove single product from Products page
-    When I add a single product to the cart
-    And I remove the product from the Products page
-    Then the cart badge should not exist
-    And the product should be removed from the cart
-
+    Given I add first product to cart
+    When I click remove button
+    Then the cart badge should not be visible
+    And the product button shows "Add to cart"
+  
   Scenario: Remove multiple products from Products page
     When I add multiple products to the cart
     And I remove some products from the Products page
     Then the cart badge should reflect the correct number of remaining products
     And the removed products should not be in the cart
-
+  
   Scenario: Remove product from Product Details page
     When I navigate to a product details page
     And I add the product to the cart
     And I remove the product from the Product Details page
-    Then the cart badge should not exist
+    Then the cart badge should no longer exist
     And the product should be removed from the cart
-
+  
   Scenario: Remove products from Shopping Cart
     When I add multiple products to the cart
     And I navigate to the Shopping Cart
     And I remove a single product from the Shopping Cart
     Then the cart badge should reflect the correct number of remaining products
     And the removed product should not be in the cart
-
+  
   Scenario: Remove all products from Shopping Cart
     When I add multiple products to the cart
     And I navigate to the Shopping Cart
     And I remove all products from the Shopping Cart
     Then the Shopping Cart should be empty
     And the cart badge should not exist
-
+  
   Scenario: Attempt to remove product from empty cart
     When I navigate to the Shopping Cart
     Then no remove buttons should be visible
--- cypress/e2e/step_definitions/remove-products-steps.js
+++ cypress/e2e/step_definitions/remove-products-steps.js
@@ -1,78 +1,143 @@
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
-import LoginPage from '../../support/pages/LoginPage';
-import ProductPage from '../../support/pages/ProductPage';
-import ProductDetailsPage from '../../support/pages/ProductDetailsPage';
-import CartPage from '../../support/pages/CartPage';
+// cypress/e2e/step_definitions/remove-products.steps.js
+const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor");
 
-Given('I am logged in as a standard user', () => {
-    LoginPage.visit();
-    LoginPage.login('standard_user', 'secret_sauce');
-    LoginPage.verifyLoginSuccess();
+// Background steps
+Given('I visit the Swag Labs homepage', () => {
+    cy.visit('/');
+    cy.get('#user-name').should('be.visible');
 });
 
-When('I add a single product to the cart', () => {
-    ProductPage.addProductToCart(0);
+Given('I log in as standard user', () => {
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.url().should('include', '/inventory.html');
 });
 
+// First scenario steps
+Given('I add first product to cart', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]')
+      .should('be.visible')
+      .and('have.text', 'Add to cart')
+      .click();
+});
+
+When('I click remove button', () => {
+    cy.get('[data-test="remove-sauce-labs-backpack"]')
+      .should('be.visible')
+      .and('have.text', 'Remove')
+      .click();
+});
+
+Then('the cart badge should not be visible', () => {
+    cy.get('.shopping_cart_badge')
+      .should('not.exist');
+});
+
+Then('the product button shows {string}', (text) => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]')
+      .should('be.visible')
+      .and('have.text', text);
+});
+
+// Remove multiple products from Products page scenario steps
 When('I add multiple products to the cart', () => {
-    ProductPage.addProductToCart(0);
-    ProductPage.addProductToCart(1);
-    ProductPage.addProductToCart(2);
-});
-
-When('I remove the product from the Products page', () => {
-    ProductPage.removeProductFromCart(0);
+    // Add multiple products to the cart
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
 });
 
 When('I remove some products from the Products page', () => {
-    ProductPage.removeProductFromCart(0);
-    ProductPage.removeProductFromCart(1);
-});
-
-When('I navigate to a product details page', () => {
-    ProductPage.navigateToProductDetails(0);
-});
-
-When('I add the product to the cart', () => {
-    ProductDetailsPage.addToCart();
-});
-
-When('I remove the product from the Product Details page', () => {
-    ProductDetailsPage.removeFromCart();
-});
-
-When('I navigate to the Shopping Cart', () => {
-    ProductPage.navigateToCart();
-});
-
-When('I remove a single product from the Shopping Cart', () => {
-    CartPage.removeProduct(0);
-});
-
-When('I remove all products from the Shopping Cart', () => {
-    CartPage.removeAllProducts();
-});
-
-Then('the cart badge should not exist', () => {
-    ProductPage.verifyCartBadgeNotExists();
-});
-
-Then('the product should be removed from the cart', () => {
-    ProductPage.verifyProductNotInCart();
+    // Remove specific products from the cart
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+    cy.get('[data-test="remove-sauce-labs-bike-light"]').click();
 });
 
 Then('the cart badge should reflect the correct number of remaining products', () => {
-    ProductPage.verifyCartBadgeCount();
+    // Assert that the cart badge shows the correct count
+    cy.get('.shopping_cart_badge')
+      .should('have.text', '1');
 });
 
 Then('the removed products should not be in the cart', () => {
-    ProductPage.verifyRemovedProductsNotInCart();
+    // Navigate to the cart page
+    cy.get('.shopping_cart_link').click();
+
+    // Assert that the removed products are not present in the cart
+    cy.get('.cart_item')
+      .should('have.length', 1)
+      .and('not.contain', 'Sauce Labs Backpack')
+      .and('not.contain', 'Sauce Labs Bike Light');
+});
+
+// Remove product from Product Details page scenario steps
+When('I navigate to a product details page', () => {
+    // Click on the product name to navigate to its details page
+    cy.get('[data-test="inventory-item-name"]').contains('Sauce Labs Backpack').click();
+});
+
+When('I add the product to the cart', () => {
+    // Click the "Add to cart" button on the product details page
+    cy.get('[data-test="add-to-cart"]').click();
+});
+
+When('I remove the product from the Product Details page', () => {
+    // Click the "Remove" button on the product details page
+    cy.get('[data-test="remove"]').click();
+});
+
+Then('the cart badge should no longer exist', () => {
+    // Assert that the cart badge does not exist
+    cy.get('.shopping_cart_badge').should('not.exist');
+});
+
+Then('the product should be removed from the cart', () => {
+    // Navigate to the cart page
+    cy.get('.shopping_cart_link').click();
+
+    // Assert that the cart is empty
+    cy.get('.cart_item').should('not.exist');
+});
+
+// Remove products from Shopping Cart scenario steps
+When('I navigate to the Shopping Cart', () => {
+    // Click on the shopping cart link to navigate to the cart page
+    cy.get('.shopping_cart_link').click();
+});
+
+When('I remove a single product from the Shopping Cart', () => {
+    // Remove a specific product from the cart
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+});
+
+Then('the removed product should not be in the cart', () => {
+    // Assert that the removed product is not present in the cart
+    cy.get('.cart_item')
+      .should('have.length', 2)
+      .and('not.contain', 'Sauce Labs Backpack');
+});
+
+// Remove all products from Shopping Cart scenario steps
+When('I remove all products from the Shopping Cart', () => {
+    // Remove all products from the cart
+    cy.get('[data-test^="remove-"]').each(($removeButton) => {
+        cy.wrap($removeButton).click();
+    });
 });
 
 Then('the Shopping Cart should be empty', () => {
-    CartPage.verifyCartIsEmpty();
+    // Assert that the cart is empty
+    cy.get('.cart_item').should('not.exist');
 });
 
+Then('the cart badge should not exist', () => {
+    // Assert that the cart badge does not exist
+    cy.get('.shopping_cart_badge').should('not.exist');
+});
+
+// Attempt to remove product from empty cart scenario steps
 Then('no remove buttons should be visible', () => {
-    CartPage.verifyNoRemoveButtons();
+    // Assert that no remove buttons are visible
+    cy.get('[data-test^="remove-"]').should('not.exist');
 });
